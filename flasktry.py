88c838908149f87c270f6e42afb9beb54d9845cb
Anup Mallah <80119585+mallahanup@users.noreply.github.com>
2023-12-13 23:50:30 +0530

    Update flasktry.py

--- flasktry.py
+++ flasktry.py
@@ -300,4 +300,4 @@ def predict():
 
     return render_template('index.html')
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=False)
+    app.run(host='0.0.0.0',debug=False, port=5000)
